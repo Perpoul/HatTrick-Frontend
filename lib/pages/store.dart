31e16cc742bc889b8ce08ab25e90d13d470b6047
benmucha <43706062+benmucha@users.noreply.github.com>
2023-04-17 15:53:04 -0400

    Add Shop Item Styling

--- lib/pages/store.dart
+++ lib/pages/store.dart
@@ -6,7 +6,8 @@ class Store extends StatefulWidget {
 }
 
 class _StoreState extends State<Store> {
-  final List<IconImg> storeImgNames = [IconImg("default.png"), IconImg("bangs.png"), IconImg("hat.png"), IconImg("long.png")]; // IconImg("hat.png", 100), 
+  final List<IconImg> storeImgNames = [IconImg("default.png", "FREE"), IconImg("bangs.png", "5"), IconImg("hat.png", "10"), IconImg("long.png", "20")];
+  final bool a = false; 
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +26,62 @@ class _StoreState extends State<Store> {
       body: GridView.count(
           crossAxisCount: 3,
           children: List.generate(storeImgNames.length, (index) {
-            return Center(
+            return Card(
+              margin: EdgeInsets.all(8),
+              elevation: 5,
+              shape: storeImgNames[index].isEquipped() ? RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6.0),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 2.0,
+                ),
+              ) : RoundedRectangleBorder(borderRadius: BorderRadius.circular(6.0)),
+              child: Stack(
+                children: <Widget>[
+                  Center(
                 child: Image(
-                        image: AssetImage("assets/${storeImgNames[index].imgName}"),
-                        width: storeImgNames[index].height,
-                        fit: BoxFit.cover
-                      ));
+                      image: AssetImage("assets/${storeImgNames[index].imgName}"),
+                      width: storeImgNames[index].width,
+                      fit: BoxFit.cover
+                    )),
+                  Positioned(
+                    bottom: 3,
+                    right: 3,
+                    child: Container(
+                      decoration: const BoxDecoration (
+                        color: Color.fromARGB(204, 158, 158, 158),
+                        borderRadius: BorderRadius.all(Radius.circular(5))
+                      ),
+                      padding: EdgeInsets.fromLTRB(0, 0, 2, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Icon(
+                            Icons.attach_money,
+                            size: 25,
+                            color: Color(0xFFFFD700),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.zero,
+                            child: Container(
+                              transform: Matrix4.translationValues(-3.0, 0, 0.0),
+                              child: Text(
+                              storeImgNames[index].priceLabel,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFFFFD700),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
           })),
     );
   }
@@ -38,7 +89,13 @@ class _StoreState extends State<Store> {
 
 class IconImg {
   String imgName;
-  double height = 100;
-  IconImg(this.imgName);
-  IconImg.sized(this.imgName, this.height);
+  String priceLabel;
+  double width = 100;
+
+  IconImg(this.imgName, this.priceLabel);
+  IconImg.sized(this.imgName, this.priceLabel, this.width);
+
+  bool isEquipped(){
+    return imgName == "default.png";
+  }
 }
